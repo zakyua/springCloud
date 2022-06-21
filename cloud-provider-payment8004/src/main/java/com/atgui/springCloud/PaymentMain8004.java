310d66c9e199cb36366ced895d80857cb3f473ed
Chen <chen@163.com>
2022-06-21 21:28:34 +0800

    使用zookeeper做注册中心的消费者

---
+++ cloud-provider-payment8004/src/main/java/com/atgui/springCloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.atgui.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author ChenCheng
+ * @create 2022-06-21 20:53
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
