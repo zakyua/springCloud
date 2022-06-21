50059416ac6d08cdf689d16acf1ee714a8d7ee5c
Chen <chen@163.com>
2022-06-21 21:30:19 +0800

    Eureka注册中心集群7001

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springCloud/EurekaMain7001.java
@@ -0,0 +1,22 @@
+package com.atguigu.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务治理
+ * 在传统的RPC远程调用框架中，管理每个服务与服务之间依赖关系比较复杂，管理比较复杂，
+ * 所以需要使用服务治理，管理服务于服务之间依赖关系，可以实现服务调用、负载均衡、容错等，实现服务发现与注册。
+ * @author ChenCheng
+ * @create 2022-06-21 18:37
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
+
+// CLOUD-PAYMENT-SERVICE
