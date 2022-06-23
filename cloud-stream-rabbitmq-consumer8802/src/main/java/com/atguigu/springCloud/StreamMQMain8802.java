0b3e598fb9d18b1952d207cc91b8391bb6ed6e0e
Chen <chen@163.com>
2022-06-23 13:59:19 +0800

    使用stream的消息消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springCloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.atguigu.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ChenCheng
+ * @create 2022-06-23 13:51
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
